4f0e0b7aa095c7dd28f2d4e7e1b79958ab1cd33c
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2022-06-27 13:31:24 -0600

    completed all objectives for react router

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+    return (
+      <nav>
+      <Link to="/" style={{color : "yellow"}}>Home </Link>
+      <Link to="/blogs" style={{color : "green"}}>Blogs</Link>
+      </nav>
+    );
+  };
+  
+  export default Navbar;
